ac2686d1f295f49f4603d26e592c75e73d0f53cd
sidharth <sidharthm673@gmail.com>
2024-07-17 16:54:08 +0530

    setting up the roject

--- src/main/java/com/sid/aopdemo/AopdemoApplication.java
+++ src/main/java/com/sid/aopdemo/AopdemoApplication.java
@@ -1,7 +1,9 @@
 package com.sid.aopdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopdemoApplication {
@@ -10,4 +12,10 @@ public class AopdemoApplication {
 		SpringApplication.run(AopdemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(String[] args){
+		return runner ->{
+			System.out.println("Hello World");
+		};
+	}
 }
