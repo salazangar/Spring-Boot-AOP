38ef95de50fc2b3ff6e1725805a6b5d4be201f24
sidharth <sidharthm673@gmail.com>
2024-07-17 19:34:34 +0530

    the advice matches any addAccount()

--- src/main/java/com/sid/aopdemo/AopdemoApplication.java
+++ src/main/java/com/sid/aopdemo/AopdemoApplication.java
@@ -1,6 +1,7 @@
 package com.sid.aopdemo;
 
 import com.sid.aopdemo.dao.AccountDAO;
+import com.sid.aopdemo.dao.MembershipDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,18 +15,21 @@ public class AopdemoApplication {
 	}
 
 	@Bean
-	CommandLineRunner commandLineRunner(AccountDAO accountDAO){
+	CommandLineRunner commandLineRunner(AccountDAO accountDAO, MembershipDAO membershipDAO){
 		return runner ->{
 
-			demoTheBeforeAdvice(accountDAO);
+			demoTheBeforeAdvice(accountDAO, membershipDAO);
 
 		};
 	}
 
-	private void demoTheBeforeAdvice(AccountDAO accountDAO) {
+	private void demoTheBeforeAdvice(AccountDAO accountDAO, MembershipDAO membershipDAO) {
 
 		// call the business method
 		accountDAO.addAccount();
+
+		// call the membership business method
+		membershipDAO.addAccount();
 
 	}
 }
---
+++ src/main/java/com/sid/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,6 @@
+package com.sid.aopdemo.dao;
+
+public interface MembershipDAO {
+
+    void addAccount();
+}
